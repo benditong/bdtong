6bbe0ca0227075084372784139be51a2533d78b7
wyxhome <35700424@qq.com>
2015-09-09 14:07:33 +0800

    wyx 2015-09-09

--- benditong/src/com/zykj/benditong/activity/CarpoolMainActivity.java
+++ benditong/src/com/zykj/benditong/activity/CarpoolMainActivity.java
@@ -2,14 +2,19 @@ package com.zykj.benditong.activity;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.http.Header;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -20,10 +25,12 @@ import com.zykj.benditong.R;
 import com.zykj.benditong.adapter.CarpoolAdapter;
 import com.zykj.benditong.http.HttpUtils;
 import com.zykj.benditong.model.Car;
+import com.zykj.benditong.utils.StringUtil;
 import com.zykj.benditong.view.XListView;
+import com.zykj.benditong.view.XListView.IXListViewListener;
 
-public class CarpoolMainActivity extends BaseActivity {
-	private static int PERPAGE = 5;// perpage默认每页显示5条信息
+public class CarpoolMainActivity extends BaseActivity implements IXListViewListener,OnItemClickListener{
+	private static int PERPAGE = 2;// perpage默认每页显示5条信息
 	private int nowpage = 1;// 当前显示的页面
 	private Button btn_carpoolNeeder, btn_carpoolOwner;
 	private ImageButton btn_carpoolBack;
@@ -36,6 +43,7 @@ public class CarpoolMainActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.setContentView(R.layout.ui_carpool);
+		mHandler = new Handler();
 		initView();
 		getShopListData();
 	}
@@ -47,6 +55,8 @@ public class CarpoolMainActivity extends BaseActivity {
 		car_listView = (XListView) findViewById(R.id.car_listview);
 		car_listView.setPullLoadEnable(true);
 		car_listView.setPullRefreshEnable(true);
+		car_listView.setXListViewListener(this);
+		car_listView.setOnItemClickListener(this);
 		setListener(btn_carpoolBack, btn_carpoolNeeder, btn_carpoolOwner);
 	}
 
@@ -65,16 +75,47 @@ public class CarpoolMainActivity extends BaseActivity {
 				JSONObject datas = jsonObject.getJSONObject("datas");
 				String jsonArray = datas.getString("list");
 				List<Car> list = JSONArray.parseArray(jsonArray, Car.class);
-
-				adapter = new CarpoolAdapter(CarpoolMainActivity.this, list);
+				cars.clear();//清楚列表数据
+				cars.addAll(list);
+				adapter = new CarpoolAdapter(CarpoolMainActivity.this, cars);
 				car_listView.setAdapter(adapter);
+				
+			}
 
-//				if (nowpage == 1) {
-//					cars.clear();
-//				}
-//				cars.addAll(list);
-//				adapter.notifyDataSetChanged();
+			@Override
+			public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+					Throwable arg3) {
+				/** 访问失败 */
+				Toast.makeText(CarpoolMainActivity.this, "请求失败",
+						Toast.LENGTH_LONG).show();
+				// Tools.toast(CarpoolMainActivity.this, "请求失败");
+			}
+		}, params);
 
+	}
+
+	private void loadMoreData() {
+		RequestParams params = new RequestParams();
+		params.put("type", "need");
+		params.put("nowpage", nowpage);// 第几页
+		params.put("perpage", PERPAGE);// 每页条数
+		HttpUtils.getList(new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(int statusCode, Header[] header,
+					byte[] stringjson) {
+				/** 访问成功 */
+				String jsonString = new String(stringjson);
+				JSONObject jsonObject = (JSONObject) JSON.parse(jsonString);
+				
+				JSONObject datas = jsonObject.getJSONObject("datas");
+				String jsonArray = datas.getString("list");
+				if(!StringUtil.isEmpty(jsonArray)){
+					List<Car> list = JSONArray.parseArray(jsonArray, Car.class);
+					cars.addAll(list);
+					adapter = new CarpoolAdapter(CarpoolMainActivity.this, cars);
+					car_listView.setAdapter(adapter);
+					adapter.notifyDataSetChanged();
+				}
 			}
 
 			@Override
@@ -104,41 +145,49 @@ public class CarpoolMainActivity extends BaseActivity {
 		}
 	}
 
-//	public void onRefresh() {
-//		try {
-//			mHandler.postDelayed(new Runnable() {
-//				@Override
-//				public void run() {
-//					nowpage = 1;
-//					getShopListData();
-//					onLoad();
-//				}
-//			}, 1000);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	
-//	}
-//
-//	// 上拉加载分页
-//	public void onLoadMore() {
-//		try {
-//			mHandler.postDelayed(new Runnable() {
-//				@Override
-//				public void run() {
-//					nowpage += 1;
-//					getShopListData();
-//					onLoad();
-//				}
-//			}, 1000);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	private void onLoad() {
-//		car_listView.stopRefresh();
-//		car_listView.stopLoadMore();
-//		car_listView.setRefreshTime("刚刚");
-//	}
+	@Override
+	 public void onRefresh() {
+		 try {
+			 mHandler.postDelayed(new Runnable() {
+				 @Override
+				 public void run() {
+					 nowpage = 1;
+					 getShopListData();
+					 onLoad();
+				 }
+			 }, 1000);
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }
+	 }
+	
+	 // 上拉加载分页
+	@Override
+	public void onLoadMore() {
+		try {
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					nowpage += 1;
+					loadMoreData();
+					onLoad();
+				}
+
+			}, 1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void onLoad() {
+		car_listView.stopRefresh();
+		car_listView.stopLoadMore();
+		car_listView.setRefreshTime("刚刚");
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
 }
