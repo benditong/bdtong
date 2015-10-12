8b26a10dbc8e4eeabe47ad6b504409a98ec2d806
cuishenghu <351088978@qq.com>
2015-10-12 14:33:43 +0800

    csh 2015-10-09

--- benditong/src/com/zykj/benditong/activity/HouseDetailActivity.java
+++ benditong/src/com/zykj/benditong/activity/HouseDetailActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
