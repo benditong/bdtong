8cce629ee1d5fcaac5c92e53c5c8d2f024b7cfda
wyxhome <35700424@qq.com>
2015-10-13 16:08:06 +0800

    wyx 2015-10-13

--- benditong/src/com/zykj/benditong/activity/AppAboutActivity.java
+++ benditong/src/com/zykj/benditong/activity/AppAboutActivity.java
@@ -7,8 +7,8 @@ import android.text.Html;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.zykj.benditong.BaseActivity;
@@ -17,19 +17,18 @@ import com.zykj.benditong.view.MyCommonTitle;
 
 public class AppAboutActivity extends BaseActivity{
 	private MyCommonTitle myCommonTitle; 
-	private ImageButton mCallButton;
+	private ImageView mCallButton;
 	private TextView tv_phone,about_us;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		super.setContentView(R.layout.about_us);
+		setContentView(R.layout.about_us);
 		myCommonTitle=(MyCommonTitle) findViewById(R.id.aci_mytitle);
 		myCommonTitle.setTitle("关于我们");
 		tv_phone=(TextView) findViewById(R.id.phone);
 		about_us=(TextView) findViewById(R.id.about_us);
 		about_us.setText(Html.fromHtml(about_us.getText().toString()));
-		mCallButton=(ImageButton) findViewById(R.id.imag_buton_phone);
+		mCallButton=(ImageView) findViewById(R.id.imag_buton_phone);
 		mCallButton.setOnTouchListener(new OnTouchListener() {
 		
 			public boolean onTouch(View arg0, MotionEvent arg1) {
